d4fdce50c86a34540db0056a0b47c14783c93e03
Serhii <serhii.bondarenko@hal24k.com>
2023-11-04 20:47:22 +0200

    add a chart

--- src/components/orgChartTree/index.jsx
+++ src/components/orgChartTree/index.jsx
@@ -6,39 +6,123 @@ import Tree
 // This is a simplified example of an org chart with a depth of 2.
 // Note how deeper levels are defined recursively via the `children` property.
 const orgChart = {
-    name: 'CEO',
-    children: [
+    name: 'IT Industry',
+    'current': false,
+    'blocked': true,
+    'children': [
         {
-            name: 'Manager',
-            attributes: {
-                department: 'Production',
-            },
-            children: [
+            'name': 'Developer',
+            'current': false,
+            'blocked': false,
+            'children': [
                 {
-                    name: 'Foreman',
-                    attributes: {
-                        department: 'Fabrication',
-                    },
-                    children: [
+                    'name': 'Java',
+                    'current': false,
+                    'blocked': false,
+                    'children': [
                         {
-                            name: 'Worker',
-                        },
-                    ],
+                            'name': 'Java Principal',
+                            'current': false,
+                            'blocked': false,
+                            'children': [
+                                {
+                                    'name': 'Java Senior',
+                                    'current': false,
+                                    'blocked': false,
+                                    'children': [
+                                        {
+                                            'name': 'Java Middle',
+                                            'current': true,
+                                            'blocked': false,
+                                            'children': [
+                                                {
+                                                    'name': 'Java Junior',
+                                                    'current': false,
+                                                    'blocked': true
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
                 },
                 {
-                    name: 'Foreman',
-                    attributes: {
-                        department: 'Assembly',
-                    },
-                    children: [
+                    'name': 'Python',
+                    'current': false,
+                    'blocked': false,
+                    'children': [
                         {
-                            name: 'Worker',
-                        },
-                    ],
+                            'name': 'Python Principal',
+                            'current': false,
+                            'blocked': false,
+                            'children': [
+                                {
+                                    'name': 'Python Senior',
+                                    'current': false,
+                                    'blocked': false,
+                                    'children': [
+                                        {
+                                            'name': 'Python Middle',
+                                            'current': false,
+                                            'blocked': false,
+                                            'children': [
+                                                {
+                                                    'name': 'Python Junior',
+                                                    'current': false,
+                                                    'blocked': false
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
                 },
-            ],
+                {
+                    'name': 'JavaScript',
+                    'current': false,
+                    'blocked': false,
+                    'children': []
+                }
+            ]
         },
-    ],
+        {
+            'name': 'QA',
+            'current': false,
+            'blocked': false,
+            'children': [
+                {
+                    'name': 'QA Principal',
+                    'current': false,
+                    'blocked': false,
+                    'children': [
+                        {
+                            'name': 'QA Senior',
+                            'current': true,
+                            'blocked': false,
+                            'children': [
+                                {
+                                    'name': 'QA Middle',
+                                    'current': false,
+                                    'blocked': false,
+                                    'children': [
+                                        {
+                                            'name': 'QA Junior',
+                                            'current': false,
+                                            'blocked': false
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 }
 
 const OrgChartTree = () => {
@@ -47,11 +131,17 @@ const OrgChartTree = () => {
         <div
             id="treeWrapper"
             style={{
-                width: '100%',
-                height: '100%'
+                display: 'flex',
+                justifyContent: 'center', // Center horizontally
+                alignItems: 'center',     // Center vertically
+                width: '1000px',
+                height: '800px',
+                overflow: 'hidden'
             }}>
             <Tree
+                translate={{ x: 0, y: 0 }}
                 orientation="vertical"
+                zoom={1}
                 data={orgChart}/>
         </div>
     )
