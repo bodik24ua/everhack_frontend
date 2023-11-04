062941d62e2c48e415313d97be12403fea4b1946
Bogdan Sydorenko <bodik24ua@gmail.com>
2023-11-05 00:07:13 +0200

    make better

--- src/pages/pageNotFound/index.jsx
+++ src/pages/pageNotFound/index.jsx
@@ -1,8 +1,82 @@
+import './404.css';
+
 const PageNotFound = () => {
     return (
-        <div>
-            <h2>404 Page not found</h2>
-        </div>
+        <>
+  <div className="page">
+  <div className="error">
+    <div className="wrap">
+      <div className={404}>
+        <pre>
+          <code>
+            {"\n"}
+            {"\t"} <span className="green">&lt;!</span>
+            <span>EverHack</span>
+            <span className="green">&gt;</span>
+            {"\n"}
+            <span className="orange">&lt;html&gt;</span>
+            {"\n"}
+            {"    "}
+            <span className="orange">&lt;style&gt;</span>
+            {"\n"}
+            {"   "}* {"{"}
+            {"\n"}
+            {"\t"}
+            {"\t"}
+            {"        "}
+            <span className="green">Trainual </span>&lt;&gt;
+            <span className="blue"> Everlabs</span>;{"\n"}
+            {"}"}
+            {"\n"}
+            {"     "}
+            <span className="orange">&lt;/style&gt;</span>
+            {"\n"} <span className="orange">&lt;body&gt;</span> {"\n"}
+            {"              "}PAGE 404!{"\n"}
+            {"\t"}
+            {"\t"}
+            {"\t"}
+            {"\t"}PAGE NOT FOUND!{"\n"}
+            {"\t"}
+            {"\t"}
+            {"\t"}
+            {"\t"}
+            <span className="comment">
+              &lt;!--The page you are looking for, {"\n"}
+              {"\t"}
+              {"\t"}
+              {"\t"}
+              {"\t"}
+              {"\t"}is not where you think it is.--&gt;{"\n"}
+              {"\t"}
+              {"\t"}
+            </span>
+            {"\n"} <span className="orange" /> {"\n"}
+            {"\t"}
+            {"\t"}
+            {"\t"}
+            {"  "}
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            {"\n"}
+            {"      "}
+          </code>
+        </pre>
+      </div>
+      <code>
+        <br />
+        <span className="info">
+          <br />
+          <span className="orange">&nbsp;&lt;/body&gt;</span>
+          <br />
+          <span className="orange">&lt;/html&gt;</span>
+        </span>
+      </code>
+    </div>
+  </div>
+  </div>
+</>
+
     );
 }
 
